f8a864c9d2ed253c6af73a51d305d39b64d49b09
Antoine Bertier <antoine.bertier@safti.fr>
2018-06-24 01:08:58 +0200

    initial game structure, to produce initial UI

--- script/LoopingLife/defines.js
+++ script/LoopingLife/defines.js
@@ -1,10 +1,94 @@
 window.defines = {
   lifeStages : {
     tutorialPhase : {
-      name : "Baby"
+      libel : 'lifeStages>tutorialPhase',
     },
     learningPhase : {
-      name : "Childhood"
-    }
+      libel : 'lifeStages>learningPhase',
+    },
+  },
+  needs : {
+    lifeMater : {
+      libel : 'needs>lifeMater',
+      mechanic : {
+        tutorialPhase : {
+          type : 'decay',
+          rate : -1,
+        },
+        learningPhase : {
+          type : 'decay',
+          rate : -2,
+        },
+      },
+      triggerCondition : function(gameState,value) {
+        return value < 0;
+      },
+    },
+  },
+  statistics : {
+    resting : {
+      libel : 'statistics>resting',
+    },
+    stamina : {
+      libel : 'statistics>stamina',
+    },
+    imagination : {
+      libel : 'statistics>imagination',
+    },
+  },
+  actions : {
+    changeLifeStage : {
+      tutorialPhase : {
+        libel : 'actions>tutorialPhase>changeLifeStage',
+        needs : {
+          lifeMater : 100,
+        },
+        duration : 15,
+        statistics : {
+          stamina : 1,
+        },
+        reward : function(gameState) {
+          return {
+            type : 'progressPhase',
+            step : 'learningPhase',
+          };
+        },
+      },
+    },
+    resting : {
+      tutorialPhase : {
+        libel : 'actions>tutorialPhase>resting',
+        needs : {},
+        duration : 5,
+        statistics : {
+          resting : 0.9,
+          stamina : 0.1,
+        },
+        reward : function(gameState) {
+          return {
+            type : 'earnNeed',
+            need : 'lifeMater',
+            value : 20,
+          };
+        },
+      },
+      learningPhase : {
+        libel : 'actions>learningPhase>resting',
+        needs : {},
+        duration : 20,
+        statistics : {
+          resting : 0.8,
+          stamina : 0.1,
+          imagination : 0.1,
+        },
+        reward : function(gameState) {
+          return {
+            type : 'earnNeed',
+            need : 'lifeMater',
+            value : 15,
+          };
+        },
+      },
+    },
   },
 }
