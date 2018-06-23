97ba224548dc425c2e75876afb28971b4dff32e7
Antoine Bertier <antoine.bertier@safti.fr>
2018-06-24 01:36:42 +0200

    Shaping tutorialPhase actions and needs

--- script/LoopingLife/defines.js
+++ script/LoopingLife/defines.js
@@ -24,17 +24,37 @@ window.defines = {
         return value < 0;
       },
     },
+    enjoyment : {
+      libel : 'needs>enjoyment',
+      mechanic : {
+        tutorialPhase : {
+          type : 'tendsTo',
+          method : 'percent',
+          rate : 0.01,
+          goal : 0
+        },
+        learningPhase : {
+          type : 'tendsTo',
+          method : 'percent',
+          rate : 0.05,
+          goal : 0
+        },
+      },
+      triggerCondition : function(gameState,value) {
+        return false;
+      },
+    },
   },
   statistics : {
-    resting : {
-      libel : 'statistics>resting',
-    },
     stamina : {
       libel : 'statistics>stamina',
     },
     imagination : {
       libel : 'statistics>imagination',
     },
+    charisma : {
+      libel : 'statistics>charisma',
+    }
   },
   actions : {
     changeLifeStage : {
@@ -42,6 +62,7 @@ window.defines = {
         libel : 'actions>tutorialPhase>changeLifeStage',
         needs : {
           lifeMater : 100,
+          enjoyment : 20,
         },
         duration : 15,
         statistics : {
@@ -55,14 +76,53 @@ window.defines = {
         },
       },
     },
+    crying : {
+      tutorialPhase : {
+        libel : 'actions>tutorialPhase>crying',
+        needs : {
+          lifeMater : 10,
+        },
+        duration : 10,
+        statistics : {
+          stamina : 0.7,
+          charisma : 0.3,
+        },
+        reward : function(gameState) {
+          return {
+            type : 'earnNeed',
+            need : 'enjoyment',
+            value : -10,
+          };
+        },
+      },
+    },
+    playing : {
+      tutorialPhase : {
+        libel : 'actions>tutorialPhase>playing',
+        needs : {
+          lifeMater : 10,
+        },
+        duration : 10,
+        statistics : {
+          stamina : 0.3,
+          imagination : 0.7,
+        },
+        reward : function(gameState) {
+          return {
+            type : 'earnNeed',
+            need : 'enjoyment',
+            value : 10,
+          };
+        },
+      },
+    },
     resting : {
       tutorialPhase : {
         libel : 'actions>tutorialPhase>resting',
         needs : {},
         duration : 5,
         statistics : {
-          resting : 0.9,
-          stamina : 0.1,
+          stamina : 1,
         },
         reward : function(gameState) {
           return {
@@ -77,8 +137,7 @@ window.defines = {
         needs : {},
         duration : 20,
         statistics : {
-          resting : 0.8,
-          stamina : 0.1,
+          stamina : 0.9,
           imagination : 0.1,
         },
         reward : function(gameState) {
